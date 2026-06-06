dcdbc52f87217c2291da50a2b66f6cd5c47d0d25
Riya Sharma <riyasharmaptn1717@gmail.com>
2026-06-06 21:29:57 +0530

    feat: integrate Gemini API and answer generation pipeline

--- llm.py
+++ llm.py
@@ -1,6 +1,33 @@
-class LLM:
-    def __init__(self):
-        pass
+import google.generativeai as genai
+from dotenv import load_dotenv   #Loads variables from .env
+import os       #Lets us access environment variables
 
-    def answer(self, query: str, context: str):
-        return f"This is a placeholder answer for query: '{query}' with context: '{context}'"
+load_dotenv()  #Reads:.env and loads its contents.
+
+api_key = os.getenv("GEMINI_API_KEY")  #gets key from the environment
+
+if not api_key:
+    raise ValueError("GEMINI_API_KEY not found in .env file")
+
+genai.configure(api_key=api_key) #Configure the Gemini library using the API key stored in the variable api_key
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+def generate_answer(question, context):
+    prompt = f"""
+    Context:
+    {context}
+
+    Question:
+    {question}
+
+    Answer the question using only the provided context.
+    """
+
+    try:
+        response = model.generate_content(prompt) #An object containing Gemini's generated output.
+        return response.text
+
+    except Exception as e:
+        print(e)
+        return "Failed to generate answer."
